bb7ab1401675a9cd68b88887a0046f727bc4cbc3
Zain Afzal <zain.afz@gmail.com>
2024-01-14 18:18:31 +1100

    Ball ui works

--- index.ts
+++ index.ts
@@ -28,64 +28,91 @@ const analytics = getAnalytics(app);
 
 // Initalize phaser.
 class Main extends Scene {
-    private cursors: Phaser.Types.Input.Keyboard.CursorKeys | null = null;
-    private ball: Phaser.Types.Physics.Matter.MatterBody | null = null;
-    
-
+    /** The ball controlled by the current local player */
+    private localBall: Phaser.Physics.Matter.Image | null = null;
     private arrow: GameObjects.Image | null = null;
+    private dragCircle: GameObjects.Graphics | null = null;
+    private lastUserInput: { direction: Phaser.Math.Vector2, force: number } | null = null;
+    private dragCircleRadius = 50;
 
     preload() {
         this.load.image('bg', 'assets/bg.png');
         this.load.image('ball', 'assets/ball.png');
         this.load.image('arrow', 'assets/arrow.png');
-        
+
     }
 
     create() {
 
         const bg = this.add.image(400, 300, 'bg');
         bg.setScale(.5);
-        this.ball = this.matter.add.image(400, 300, 'ball');
-        //this.ball.setCollideWorldBounds(true);
+        this.localBall = this.matter.add.image(400, 300, 'ball');
 
-        this.cursors = this.input.keyboard!.createCursorKeys();
+        this.dragCircle = this.add.graphics();
+        this.dragCircle.lineStyle(2, 0xffffff, 1);
+        this.dragCircle.strokeCircle(0, 0, this.dragCircleRadius);
+        this.dragCircle.setVisible(false);
 
         this.arrow = this.add.image(400, 300, 'arrow');
         this.arrow.setVisible(false);
-        
+        this.arrow.setOrigin(0.5, 1);
+    }
+
+    drawUI() {
+        if (!this.localBall || !this.arrow || !this.dragCircle) {
+            return;
+        }
+        const center = new Phaser.Math.Vector2(
+            this.input.mousePointer.downX, this.input.mousePointer.downY);
+        const direction = new Phaser.Math.Vector2(
+            this.input.mousePointer.x,
+            this.input.mousePointer.y).subtract(center);
+        const force = Math.min(
+            this.input.mousePointer.getDistance() / this.dragCircleRadius, 1)
+        this.dragCircle.x = center.x;
+        this.dragCircle.y = center.y;
+
+        const arrowScale = .5 + force;
+        const arrowRotation = Phaser.Math.Angle.BetweenPoints(
+            // Vector pointing straight up.
+            { x: 0, y: -1 },
+            direction
+        );
+
+        this.arrow.setScale(arrowScale);
+        this.arrow.setRotation(arrowRotation - Math.PI / 2);
+        this.dragCircle.setVisible(true);
+        this.arrow!.setVisible(true);
+        this.lastUserInput = { direction, force };
+    }
+
+    hideUI() {
+        this.dragCircle!.setVisible(false);
+        this.arrow!.setVisible(false);
     }
 
     update(time: number, delta: number) {
         if (this.input.mousePointer.isDown) {
-            // Just take the drag delta x and use that to compute the rotation,
-            // this is terrible ux but it works.
-            let d = this.input.mousePointer.getDistanceX() / 200;
-            if (this.input.mousePointer.downX > this.input.mousePointer.x) {
-                d = d * -1;
-            }
-            let r = d * 2 * Math.PI;
-            const scaleFactor = (Phaser.Math.Clamp(
-                Math.abs(this.input.mousePointer.getDistanceY()),
-                50,
-                200
-            ) - 50) / 150;
-
-            this.arrow!.setScale(.6 + scaleFactor);
-            this.arrow!.rotation = r;
-            const arrowHeight = 128 * (.6 + scaleFactor);
-            const p = Phaser.Math.RotateAround(
-                { x: this.ball!.x, y: this.ball!.y - (arrowHeight / 2) - 10 },
-                this.ball!.x,
-                this.ball!.y,
-                r
-            );
-            this.arrow!.x = p.x;
-            this.arrow!.y = p.y;
-            this.arrow!.setVisible(true);
+            this.drawUI();
         } else {
-            // Do not.
-            this.arrow!.setVisible(false);
+            // If the mouse was held down for more then 200ms register a hit.
+            if (this.input.mousePointer.getDuration() > 200) {
+                const direction = { x: 0, y: 0 };
+                const force = 0;
+                this.hitBall(this.lastUserInput!.direction, this.lastUserInput!.force);
+            }
+            this.hideUI();
         }
+    }
+
+    /**
+     * Hit the ball towards `direction` with a force of `force`.
+     * Force is a number between 0 and 1 with 0 representing the _minimum_ force
+     * and 1 representating the maximum. A force of 0 simply means to use the
+     * minimum force, not that no force should be applied.
+     */
+    hitBall(direction: Phaser.Math.Vector2, force: number) {
+        // TODO
     }
 }
 
@@ -112,11 +139,11 @@ async function onAuthChange(user: User | null) {
         const playerID = user.uid;
         const playerRef = ref(getDatabase(), 'players/' + playerID);
         const allPlayers = ref(getDatabase(), 'players')
-        onChildAdded(allPlayers, (snapshot) => { 
-            if(snapshot.val().id == playerID){
+        onChildAdded(allPlayers, (snapshot) => {
+            if (snapshot.val().id == playerID) {
                 //this is the player add + create local ref of ball
             }
-            else{
+            else {
                 //this is the opponent add to ref of opponent
             }
         });
